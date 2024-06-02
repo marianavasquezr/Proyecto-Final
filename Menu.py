d2b4d0327591cf21ee687f0896dec92908487e84
mateorh22 <mateo.restrepo5@udea.edu.co>
2024-06-02 12:07:46 -0500

    creamos el menu

---
+++ Menu.py
@@ -0,0 +1,9 @@
+from controlador import PacienteControlador, Controlador
+from vista import View
+
+if __name__ == "__main__":
+    paciente_controlador = PacienteControlador()
+    usuario_controlador = Controlador()
+    vista = View(paciente_controlador, usuario_controlador)
+    vista.run()
+
